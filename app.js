616924c8b2853579f4d50ca62ef136a3cccc02eb
Paul Gutierrez <gutierrez.paul@pucp.pe>
2022-12-03 14:45:39 -0500

    project in github

---
+++ app.js
@@ -0,0 +1,38 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('express-async-errors')
+const loginRouter = require('./controllers/login')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/api/login', loginRouter)
+app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor, blogsRouter)
+app.use('/api/users', middleware.tokenExtractor, middleware.userExtractor, usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
